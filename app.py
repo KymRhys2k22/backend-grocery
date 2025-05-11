f1dbf32c6e643e82377abb72dac16c66b25b8fc7
KymRhys2@k22 <kymrhys@gmail.com>
2025-05-11 22:53:32 +0800

    update CRUD 2

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ CORS(app)
 grocery_items = []
 item_id = 1
 
+@app.route("/items", methods=["GET"])
+def get_items():
+    return jsonify(grocery_items)
+
+@app.route("/items", methods=["POST"])
+def add_item():
+    global item_id
+    data = request.json
+    item = {
+        "id": item_id,
+        "name": data["name"],
+        "quantity": data["quantity"],
+        "price": data["price"]
+    }
+    grocery_items.append(item)
+    item_id += 1
+    return jsonify(item), 201
 
 @app.route("/items/<int:item_id>", methods=["PUT"])
 def update_item(item_id):
@@ -24,7 +41,6 @@ def delete_item(item_id):
     global grocery_items
     grocery_items = [item for item in grocery_items if item["id"] != item_id]
     return '', 204
-
 
 if __name__ == "__main__":
     app.run()
